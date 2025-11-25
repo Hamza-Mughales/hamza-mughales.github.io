6b9f07940112f03c0d7d6137a9b04b88e9d44b50
Hamza Mughales <hamzawemughales@gmail.com>
2025-11-25 21:28:13 +0300

    clean up css and js

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -12,27 +12,36 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-// smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
-
-        if (this.hash !== "") {
-
-            event.preventDefault();
-
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
+// Navbar toggle - Pure JavaScript (no jQuery)
+document.addEventListener('DOMContentLoaded', function() {
+    const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.querySelector('ul.nav');
+    
+    if (navToggle && navMenu) {
+        navToggle.addEventListener('click', function() {
+            this.classList.toggle('is-active');
+            navMenu.classList.toggle('show');
+        });
+        
+        // Close menu when clicking on a link
+        const navLinks = navMenu.querySelectorAll('.link');
+        navLinks.forEach(link => {
+            link.addEventListener('click', function() {
+                navToggle.classList.remove('is-active');
+                navMenu.classList.remove('show');
             });
-        } 
-    });
-});
-
-// navbar toggle
-$('#nav-toggle').click(function(){
-    $(this).toggleClass('is-active')
-    $('ul.nav').toggleClass('show');
+        });
+    }
+    
+    // Sticky navbar on scroll
+    const navbar = document.querySelector('.custom-navbar');
+    if (navbar) {
+        window.addEventListener('scroll', function() {
+            if (window.scrollY > 20) {
+                navbar.classList.add('affix');
+            } else {
+                navbar.classList.remove('affix');
+            }
+        });
+    }
 });
